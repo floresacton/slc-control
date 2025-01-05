8c61540b7784d367bc44bad0c36da8e917054cd3
Miguel Flores-Acton <mfacton1@gmail.com>
2025-01-05 01:49:09 -0500

    Update fimware from old project

--- firmware/slc-control/Core/Inc/main.h
+++ firmware/slc-control/Core/Inc/main.h
@@ -57,6 +57,60 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define BTN3_Pin GPIO_PIN_13
+#define BTN3_GPIO_Port GPIOC
+#define BTN2_Pin GPIO_PIN_14
+#define BTN2_GPIO_Port GPIOC
+#define BTN1_Pin GPIO_PIN_15
+#define BTN1_GPIO_Port GPIOC
+#define SIG3_Pin GPIO_PIN_0
+#define SIG3_GPIO_Port GPIOA
+#define SIG2_Pin GPIO_PIN_1
+#define SIG2_GPIO_Port GPIOA
+#define CSP_Pin GPIO_PIN_2
+#define CSP_GPIO_Port GPIOA
+#define SIG1_Pin GPIO_PIN_3
+#define SIG1_GPIO_Port GPIOA
+#define CSG_Pin GPIO_PIN_4
+#define CSG_GPIO_Port GPIOA
+#define SCKM_Pin GPIO_PIN_5
+#define SCKM_GPIO_Port GPIOA
+#define MISOM_Pin GPIO_PIN_6
+#define MISOM_GPIO_Port GPIOA
+#define MOSIM_Pin GPIO_PIN_7
+#define MOSIM_GPIO_Port GPIOA
+#define INTG_Pin GPIO_PIN_0
+#define INTG_GPIO_Port GPIOB
+#define INTG_EXTI_IRQn EXTI0_IRQn
+#define PPS_Pin GPIO_PIN_1
+#define PPS_GPIO_Port GPIOB
+#define PPS_EXTI_IRQn EXTI1_IRQn
+#define DRDYP_Pin GPIO_PIN_2
+#define DRDYP_GPIO_Port GPIOB
+#define DRDYP_EXTI_IRQn EXTI2_IRQn
+#define GTX_Pin GPIO_PIN_10
+#define GTX_GPIO_Port GPIOB
+#define GRX_Pin GPIO_PIN_11
+#define GRX_GPIO_Port GPIOB
+#define DC_Pin GPIO_PIN_12
+#define DC_GPIO_Port GPIOB
+#define SCKD_Pin GPIO_PIN_13
+#define SCKD_GPIO_Port GPIOB
+#define CSD_Pin GPIO_PIN_14
+#define CSD_GPIO_Port GPIOB
+#define MOSID_Pin GPIO_PIN_15
+#define MOSID_GPIO_Port GPIOB
+#define DRDYM_Pin GPIO_PIN_10
+#define DRDYM_GPIO_Port GPIOA
+#define DRDYM_EXTI_IRQn EXTI15_10_IRQn
+#define DRV1_Pin GPIO_PIN_4
+#define DRV1_GPIO_Port GPIOB
+#define DRV2_Pin GPIO_PIN_5
+#define DRV2_GPIO_Port GPIOB
+#define REPH_Pin GPIO_PIN_6
+#define REPH_GPIO_Port GPIOB
+#define BTN4_Pin GPIO_PIN_8
+#define BTN4_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
--- firmware/slc-control/Core/Src/stm32g4xx_it.c
+++ firmware/slc-control/Core/Src/stm32g4xx_it.c
@@ -55,7 +55,16 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
-
+extern PCD_HandleTypeDef hpcd_USB_FS;
+extern COMP_HandleTypeDef hcomp1;
+extern COMP_HandleTypeDef hcomp2;
+extern COMP_HandleTypeDef hcomp3;
+extern TIM_HandleTypeDef htim1;
+extern TIM_HandleTypeDef htim2;
+extern TIM_HandleTypeDef htim3;
+extern TIM_HandleTypeDef htim4;
+extern DMA_HandleTypeDef hdma_usart3_rx;
+extern UART_HandleTypeDef huart3;
 /* USER CODE BEGIN EV */
 
 /* USER CODE END EV */
@@ -197,6 +206,176 @@ void SysTick_Handler(void)
 /* For the available peripheral interrupt handler names,                      */
 /* please refer to the startup file (startup_stm32g4xx.s).                    */
 /******************************************************************************/
+
+/**
+  * @brief This function handles EXTI line0 interrupt.
+  */
+void EXTI0_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI0_IRQn 0 */
+
+  /* USER CODE END EXTI0_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(INTG_Pin);
+  /* USER CODE BEGIN EXTI0_IRQn 1 */
+
+  /* USER CODE END EXTI0_IRQn 1 */
+}
+
+/**
+  * @brief This function handles EXTI line1 interrupt.
+  */
+void EXTI1_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI1_IRQn 0 */
+
+  /* USER CODE END EXTI1_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(PPS_Pin);
+  /* USER CODE BEGIN EXTI1_IRQn 1 */
+
+  /* USER CODE END EXTI1_IRQn 1 */
+}
+
+/**
+  * @brief This function handles EXTI line2 interrupt.
+  */
+void EXTI2_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI2_IRQn 0 */
+
+  /* USER CODE END EXTI2_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(DRDYP_Pin);
+  /* USER CODE BEGIN EXTI2_IRQn 1 */
+
+  /* USER CODE END EXTI2_IRQn 1 */
+}
+
+/**
+  * @brief This function handles DMA1 channel1 global interrupt.
+  */
+void DMA1_Channel1_IRQHandler(void)
+{
+  /* USER CODE BEGIN DMA1_Channel1_IRQn 0 */
+
+  /* USER CODE END DMA1_Channel1_IRQn 0 */
+  HAL_DMA_IRQHandler(&hdma_usart3_rx);
+  /* USER CODE BEGIN DMA1_Channel1_IRQn 1 */
+
+  /* USER CODE END DMA1_Channel1_IRQn 1 */
+}
+
+/**
+  * @brief This function handles USB low priority interrupt remap.
+  */
+void USB_LP_IRQHandler(void)
+{
+  /* USER CODE BEGIN USB_LP_IRQn 0 */
+
+  /* USER CODE END USB_LP_IRQn 0 */
+  HAL_PCD_IRQHandler(&hpcd_USB_FS);
+  /* USER CODE BEGIN USB_LP_IRQn 1 */
+
+  /* USER CODE END USB_LP_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM1 update interrupt and TIM16 global interrupt.
+  */
+void TIM1_UP_TIM16_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM1_UP_TIM16_IRQn 0 */
+
+  /* USER CODE END TIM1_UP_TIM16_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim1);
+  /* USER CODE BEGIN TIM1_UP_TIM16_IRQn 1 */
+
+  /* USER CODE END TIM1_UP_TIM16_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM2 global interrupt.
+  */
+void TIM2_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM2_IRQn 0 */
+
+  /* USER CODE END TIM2_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim2);
+  /* USER CODE BEGIN TIM2_IRQn 1 */
+
+  /* USER CODE END TIM2_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM3 global interrupt.
+  */
+void TIM3_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM3_IRQn 0 */
+
+  /* USER CODE END TIM3_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim3);
+  /* USER CODE BEGIN TIM3_IRQn 1 */
+
+  /* USER CODE END TIM3_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM4 global interrupt.
+  */
+void TIM4_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM4_IRQn 0 */
+
+  /* USER CODE END TIM4_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim4);
+  /* USER CODE BEGIN TIM4_IRQn 1 */
+
+  /* USER CODE END TIM4_IRQn 1 */
+}
+
+/**
+  * @brief This function handles USART3 global interrupt / USART3 wake-up interrupt through EXTI line 28.
+  */
+void USART3_IRQHandler(void)
+{
+  /* USER CODE BEGIN USART3_IRQn 0 */
+
+  /* USER CODE END USART3_IRQn 0 */
+  HAL_UART_IRQHandler(&huart3);
+  /* USER CODE BEGIN USART3_IRQn 1 */
+
+  /* USER CODE END USART3_IRQn 1 */
+}
+
+/**
+  * @brief This function handles EXTI line[15:10] interrupts.
+  */
+void EXTI15_10_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI15_10_IRQn 0 */
+
+  /* USER CODE END EXTI15_10_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(DRDYM_Pin);
+  /* USER CODE BEGIN EXTI15_10_IRQn 1 */
+
+  /* USER CODE END EXTI15_10_IRQn 1 */
+}
+
+/**
+  * @brief This function handles COMP1, COMP2 and COMP3 interrupts through EXTI lines 21, 22 and 29.
+  */
+void COMP1_2_3_IRQHandler(void)
+{
+  /* USER CODE BEGIN COMP1_2_3_IRQn 0 */
+
+  /* USER CODE END COMP1_2_3_IRQn 0 */
+  HAL_COMP_IRQHandler(&hcomp1);
+  HAL_COMP_IRQHandler(&hcomp2);
+  HAL_COMP_IRQHandler(&hcomp3);
+  /* USER CODE BEGIN COMP1_2_3_IRQn 1 */
+
+  /* USER CODE END COMP1_2_3_IRQn 1 */
+}
 
 /* USER CODE BEGIN 1 */
 
